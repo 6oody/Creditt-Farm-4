b71f66bd41d20ac2193a57e423e532ff611498ad
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-10-29 19:24:53 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ const prefix = "B";
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('pong');
+      }
+});
+
+client.on('ready', async() => {
+var server = "505028803043655700"; // ايدي السررفر
+var channel = "505028803043655702";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**كودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودزكودز **')
+    },305);
+})
+
+client.on('ready', async() => {
+var server = "505028803043655700"; // ايدي السررفر
+var channel = "505028803043655702";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },86400);
+})
+
+client.login(process.env.BOT_TOKEN);
