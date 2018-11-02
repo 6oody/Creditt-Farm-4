54ba858866f9b65d038ce6249afcfeae3c9d7029
BoooooooooooDy.! <42409047+Booooody@users.noreply.github.com>
2018-11-02 13:22:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,5 +26,26 @@ var channel = "505028803043655702";//ايدي الروم
     client.guilds.get(server).channels.get(channel).send('#daily')
     },86400);
 })
+client.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+
+  let prefix = "^";
+  let messageArray = message.content.split (" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+
+
+
+
+
+
+  if(cmd === `${prefix}say`){
+     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Oh hell noooo oof , You don't have permission biacht");
+    let botmessage = args.join(" ");
+    message.delete().catch();
+  message.channel.send(`${botmessage}`);
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
